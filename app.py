46d2fd3772e00357f0dabbb70eeb6ab10db45102
malo21st <44739330+malo21st@users.noreply.github.com>
2023-05-31 06:50:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ if uploaded_file is not None:
     stream_handler = StreamHandler(chat_box)
     chat_llm = ChatOpenAI(model_name="gpt-3.5-turbo", streaming=True, callbacks=[stream_handler])
     qa = RetrievalQA.from_chain_type(llm=chat_llm, chain_type="stuff", retriever=vectordb.as_retriever())
-    if st.session_state.qa:
+    if st.session_state.qa["history"]:
         query = "・" + st.session_state.qa["history"][-1]["msg"]
         try:
             response = qa.run(query) # Query to ChatGPT
